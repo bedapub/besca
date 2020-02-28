f494ae34bc143c7cf3fd450babf558aa3f8625ee
nahanoo <42641685+nahanoo@users.noreply.github.com>
2020-02-28 08:27:13 +0100

    Working conda setup file

--- setup.py
+++ setup.py
@@ -1,24 +1,21 @@
-if __name__ == '__main__':
+import setuptools
 
-  from setuptools import setup, find_packages
-  with open('requirements.txt', encoding='utf-8') as requirements:
-    requires = [l.strip() for l in requirements]
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-  setup(name='besca',
-        version='2.1',
-        description='collection of BEDA internal python functions for analysing single cell RNAseq data',
-        long_description= 'please view https://pages.github.com/bedapub/besca/',
-        classifiers=[
-          'Development Status :: Release 2.1 (Scanpy 1.4.1)',
-          'Programming Language :: Python :: 3.7.1',
-          'License :: GPLv3'],
-        url='https://github.com/bedapub/besca',
-        license='GPLv3',
-        author='BEDA community',
-        author_email='alice.julien-laferriere@roche.com',
-        packages=find_packages(),
-        zip_safe=False,
-        package_data={'besca.datasets.data':['*.h5ad'], 'besca.st':['*.css'],\
-            'besca.datasets.mito_files': ['*.tsv'], 'besca.export':['reformat'],\
-            'besca.datasets.genesets':['HumanCD45p_scseqCMs6.gmt','Immune.txt']},
-        install_requires=requires)
+setuptools.setup(
+    name="besca", # Replace with your own username
+    version="2.1",
+    description="collection of BEDA internal python functions for analysing single cell RNAseq data",
+    long_description= 'please view https://pages.github.roche.com/BEDA/besca/',
+    author='BEDA community',
+    author_email='alice.julien-laferriere@roche.com',
+    url='https://github.roche.com/BEDA/besca',
+    packages=setuptools.find_packages(),
+    package_data={'besca.datasets.data':['*.h5ad'], 'besca.st':['*.css'],\
+        'besca.datasets.mito_files':['*.tsv'], 'besca.export':['reformat']},
+    classifiers=[
+        'Development Status :: Release 2.1 (Scanpy 1.4.1)',
+        'Programming Language :: Python :: 3.7.1'
+    ]
+    )
