911c1cef784d20f6e0da44b3583cf9e43a8c9e4a
Petra Schwalie <schwalip@rkalbhpc010.kau.roche.com>
2020-05-12 16:29:47 +0200

    Changes to cell type annotation notebook

--- besca/tl/sig/__init__.py
+++ besca/tl/sig/__init__.py
@@ -1,9 +1,10 @@
-from ._sig import combined_signature_score, compute_signed_score
+from ._sig import combined_signature_score, compute_signed_score, filter_siggenes
 from ._io_sig import read_GMT_sign
 from ._annot import getset, score_mw, add_anno, make_anno
 
 __all__ = ["combined_signature_score",
            "compute_signed_score",
+           "filter_siggenes",
            "read_GMT_sign",
            'getset',
            'score_mw',
