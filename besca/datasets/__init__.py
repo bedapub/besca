fa4cce3badff23397a983335caf39e5222bbe88e
hatjek <klas.hatje@roche.com>
2020-05-12 13:09:05 +0200

    Added loading methods for Martin2019 and Smillie2019 datasets.

--- besca/datasets/__init__.py
+++ besca/datasets/__init__.py
@@ -1,11 +1,15 @@
-from ._datasets import pbmc3k_raw, pbmc3k_filtered, pbmc3k_processed, pbmc_storage_raw, pbmc_storage_processed, load_immune_signatures
+from ._datasets import pbmc3k_raw, pbmc3k_filtered, pbmc3k_processed, pbmc_storage_raw, pbmc_storage_processed, Smillie2019_raw, Smillie2019_processed, Martin2019_raw, Martin2019_processed, load_immune_signatures
 from ._mito import get_mito_genes
 
 __all__ = ["pbmc3k_raw",
            "pbmc3k_filtered",
            "pbmc3k_processed",
            "pbmc_storage_raw",
-           "get_mito_genes",
            "pbmc_storage_processed",
+           "Smillie2019_raw",
+           "Smillie2019_processed",
+           "Martin2019_raw",
+           "Martin2019_processed",
+           "get_mito_genes",
            "load_immune_signatures"
            ]
