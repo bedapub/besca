bec4edbe5573547509ee904c011f44bc36c445b4
Artemis-R <sophia.maedler@gmail.com>
2020-05-20 12:06:52 +0200

    add setup_citeseq function

--- besca/st/__init__.py
+++ besca/st/__init__.py
@@ -1,5 +1,5 @@
 from ._FAIR_export import export_cp10k, export_regressedOut, export_clustering, export_metadata, export_rank, export_celltype
-from ._wrapper_funcs import setup, read_matrix, filtering_cells_genes_min, filtering_mito_genes_max, per_cell_normalize, highly_variable_genes, regress_out, batch_correction, pca_neighbors_umap, clustering, additional_labeling, celltype_labeling
+from ._wrapper_funcs import setup, setup_citeseq, read_matrix, filtering_cells_genes_min, filtering_mito_genes_max, per_cell_normalize, highly_variable_genes, regress_out, batch_correction, pca_neighbors_umap, clustering, additional_labeling, celltype_labeling
 from ._setup_funcs import create_button, create_popup
 from ._qc_report import write_qc
 
