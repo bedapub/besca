075fc99fd4bd34443def4ea28869350e9e9514a3
Alice Julien-Laferriere <alice.julien-laferriere@roche.com>
2020-05-29 13:30:17 +0200

    Renamed assert_h5ad as assert_adata. Makes more sense

--- besca/Import/__init__.py
+++ besca/Import/__init__.py
@@ -1,7 +1,7 @@
-from ._read import read_mtx, assert_h5ad
+from ._read import read_mtx, assert_adata
 from ._labelings import add_cell_labeling
 
 __all__ = ['read_mtx',
            'add_cell_labeling',
-           'assert_h5ad'
+           'assert_adata'
           ]
