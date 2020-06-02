bfc3295f2976e7a3a6067435a78ba7a13e4f9e4a
Alice Julien-Laferriere <alice.julien-laferriere@roche.com>
2020-06-02 13:18:28 +0200

    Exporting new obtain_dblabel function

--- besca/tl/sig/__init__.py
+++ besca/tl/sig/__init__.py
@@ -1,6 +1,6 @@
 from ._sig import combined_signature_score, compute_signed_score, filter_siggenes
 from ._io_sig import read_GMT_sign
-from ._annot import getset, score_mw, add_anno, make_anno, match_cluster, read_annotconfig
+from ._annot import getset, score_mw, add_anno, make_anno, match_cluster, read_annotconfig, obtain_dblabel
 
 __all__ = ["combined_signature_score",
            "compute_signed_score",
@@ -11,4 +11,5 @@ __all__ = ["combined_signature_score",
            'add_anno',
            'make_anno',
            'read_annotconfig',
-           'match_cluster']
+           'match_cluster',
+           'obtain_dblabel']
