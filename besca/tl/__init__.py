149d55c9b328bbaf88dd38cb58f23dcfaaa5a254
Alice Julien-Laferriere <alice.julien-laferriere@roche.com>
2020-06-02 13:59:20 +0200

    Code formatting: fixing imports and pep8 format in the sig modules.

--- besca/tl/__init__.py
+++ besca/tl/__init__.py
@@ -1,19 +1,15 @@
-from . import rc
-from . import dge
-from . import bcor
-from . import sig
-from . import auto_annot
-from ._count_occurrences import count_occurrence, count_occurrence_subset, count_occurrence_subset_conditions
+from . import auto_annot, bcor, dge, rc, sig
 from ._annotate_cellnames import annotate_cells_clustering
-from . import auto_annot
+from ._count_occurrences import (count_occurrence, count_occurrence_subset,
+                                 count_occurrence_subset_conditions)
 
 __all__ = ["rc",
-           "dge", 
+           "dge",
            "bcor",
            "sig",
-           "count_occurrence", 
-           "count_occurrence_subset", 
-           "count_occurrence_subset_conditions", 
+           "count_occurrence",
+           "count_occurrence_subset",
+           "count_occurrence_subset_conditions",
            "annotate_cells_clustering",
            "auto_annot"
            ]
