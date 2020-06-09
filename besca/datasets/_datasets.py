250781fac40d387496210104fa3418b9663aa73e
Alice Julien-Laferriere <alice.julien-laferriere@roche.com>
2020-06-09 15:35:23 +0200

    Update URL for pbmc3k

--- besca/datasets/_datasets.py
+++ besca/datasets/_datasets.py
@@ -39,7 +39,7 @@ def pbmc3k_raw():
     """
 
     filename = pkg_resources.resource_filename('besca', 'datasets/data/pbmc3k_raw.h5ad')
-    adata =  check_dl( filename, url = 'https://zenodo.org/record/3752813/files/pbmc3k_raw.h5ad?download=1')
+    adata =  check_dl( filename, url = 'https://zenodo.org/record/3886414/files/pbmc3k_raw.h5ad?download=1')
     return adata
 
 def pbmc3k_filtered():
@@ -62,7 +62,7 @@ def pbmc3k_filtered():
     """
 
     filename = pkg_resources.resource_filename('besca', 'datasets/data/pbmc3k_filtered.h5ad')
-    adata = check_dl( filename, url='https://zenodo.org/record/3752813/files/pbmc3k_filtered.h5ad?download=1')
+    adata = check_dl( filename, url='https://zenodo.org/record/3886414/files/pbmc3k_filtered.h5ad?download=1')
     return adata
 
 def pbmc3k_processed():
@@ -86,7 +86,7 @@ def pbmc3k_processed():
     """
 
     filename = pkg_resources.resource_filename('besca', 'datasets/data/pbmc3k_processed.h5ad')
-    adata = check_dl( filename, url='https://zenodo.org/record/3752813/files/pbmc3k_processed.h5ad?download=1')
+    adata = check_dl( filename, url='https://zenodo.org/record/3886414/files/pbmc3k_processed.h5ad?download=1')
     return adata
 
 
