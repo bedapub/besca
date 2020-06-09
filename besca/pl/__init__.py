c14bbc17a1b09c8640ec1ad667af5a426acfbbf5
nahanoo <42641685+nahanoo@users.noreply.github.com>
2020-06-09 10:04:31 +0200

    fixed import function name

--- besca/pl/__init__.py
+++ besca/pl/__init__.py
@@ -5,7 +5,7 @@ from ._qc_plots import dropouts, librarysize_overview, detected_genes, library_s
 from ._general import stacked_split_violin
 from ._dot_heatmap import dot_heatmap, dot_heatmap_split, dot_heatmap_split_greyscale
 from ._update_palette import update_qualitative_palette
-from ._nomenclature_network import plot_network
+from ._nomenclature_network import nomenclature_network
 
 __all__ = ["kp_genes", 
            "kp_counts", 
