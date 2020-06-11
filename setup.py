6c8b474ad4a046e0d0ae96e47c0f0be004685cc3
nahanoo <ulriche2@rkalbhpc010.kau.roche.com>
2020-06-11 11:47:27 +0200

    Added geneset files to setup so they come with package

--- setup.py
+++ setup.py
@@ -9,18 +9,17 @@ if __name__ == '__main__':
         version=versioneer.get_version(),
         cmdclass=versioneer.get_cmdclass(),
         description='collection of BEDA internal python functions for analysing single cell RNAseq data',
-        long_description= 'please view https://pages.github.com/bedapub/besca/',
+        long_description='please view https://pages.github.com/bedapub/besca/',
         classifiers=[
-          'Development Status :: Release 2.1.1 (Scanpy 1.4)',
-          'Programming Language :: Python :: 3.7.1',
-          'License :: GPLv3'],
+            'Development Status :: Release 2.1.1 (Scanpy 1.4)',
+            'Programming Language :: Python :: 3.7.1',
+            'License :: GPLv3'],
         url='https://github.com/bedapub/besca',
         license='GPLv3',
         author='BEDA community',
         author_email='alice.julien-laferriere@roche.com',
         packages=find_packages(),
         zip_safe=False,
-        package_data={'besca.datasets.data':['*.h5ad'], 'besca.st':['*.css'],\
-            'besca.datasets.mito_files': ['*.tsv'], 'besca.export':['reformat'],\
-            'besca.datasets.genesets':['HumanCD45p_scseqCMs6.gmt','Immune.gmt']},
+        package_data={'besca.datasets.data': ['*.h5ad'], 'besca.st': ['*.css'],
+                      'besca.datasets.mito_files': ['*.tsv'], 'besca.export': ['reformat'], 'besca.datasets.genesets': ['*.gmt', '*.tsv']},
         install_requires=requires)
