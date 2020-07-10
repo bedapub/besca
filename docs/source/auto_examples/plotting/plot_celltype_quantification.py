188ced92981e6c5bc8ed69d543da2d775688609f
Alice Julien-Laferriere <alice.julien-laferriere@roche.com>
2020-07-10 17:09:05 +0200

    fixing auto example plot_celltype_quantification

--- docs/source/auto_examples/plotting/plot_celltype_quantification.py
+++ docs/source/auto_examples/plotting/plot_celltype_quantification.py
@@ -10,28 +10,28 @@ can be used to visually represent the number of cells that belong to a certain s
 import besca as bc 
 
 #import dataset to workwith
-adata = bc.datasets.pbmc_storage_processed_bbknn()
+adata = bc.datasets.Haber2017_processed()
 
 #####################
 #quantify specific celllabels as a stacked barplot
 
-bc.pl.celllabel_quant_stackedbar(adata, count_variable = 'donor', subset_variable = 'storage_condition');
+bc.pl.celllabel_quant_stackedbar(adata, count_variable = 'donor', subset_variable = 'region');
 
 #####################
 # quantify number of cells in each louvain cluster belonging to a specific subset variable (e.g. donor)
 #
 # Note that the louvain clusters are brought into the correct order.
 
-bc.pl.louvain_quant_stackedbar(adata, subset_variable = 'donor');
+bc.pl.celllabel_quant_stackedbar(adata, count_variable = 'donor', subset_variable = 'region');
 
 #####################
 # quantify number of cells belong to each condition in a specific subset
 #
 # here each dot represents one donor, the boxplots are grouped according to storage condition
 
-bc.pl.celllabel_quant_boxplot(adata, count_variable = 'louvain', subset_variable = 'donor', condition_identifier = 'storage_condition',  plot_percentage = True);
+bc.pl.celllabel_quant_boxplot(adata, count_variable = 'leiden', subset_variable = 'donor', condition_identifier = 'region',  plot_percentage = True)
 
 #####################
 # here you can also choose to plot total counts instead of percentages
 
-bc.pl.celllabel_quant_boxplot(adata, count_variable = 'louvain', subset_variable = 'donor', condition_identifier = 'storage_condition',  plot_percentage = False);
+bc.pl.celllabel_quant_boxplot(adata, count_variable = 'leiden', subset_variable = 'donor', condition_identifier = 'region',  plot_percentage = False)
