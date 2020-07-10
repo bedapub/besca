d6a7c94090fcf9d05295c09c7dc46cc521aca10a
Alice Julien-Laferriere <alice.julien-laferriere@roche.com>
2020-07-10 17:00:49 +0200

    replacing  louvain_quant_stackedbar by leiden_quant_stackedbar and fixing examples

--- besca/pl/__init__.py
+++ besca/pl/__init__.py
@@ -1,6 +1,6 @@
 from ._filter_threshold_plots import kp_genes, kp_counts, kp_cells, max_counts, max_genes, max_mito
 from ._split_gene_expression import gene_expr_split, gene_expr_split_stacked
-from ._celltype_quantification import celllabel_quant_boxplot, celllabel_quant_stackedbar, louvain_quant_stackedbar
+from ._celltype_quantification import celllabel_quant_boxplot, celllabel_quant_stackedbar, leiden_quant_stackedbar
 from ._qc_plots import dropouts, librarysize_overview, detected_genes, library_size, transcript_capture_efficiency, top_genes_counts
 from ._general import stacked_split_violin
 from ._dot_heatmap import dot_heatmap, dot_heatmap_split, dot_heatmap_split_greyscale
@@ -24,7 +24,7 @@ __all__ = ["kp_genes",
            "stacked_split_violin", 
            "celllabel_quant_boxplot", 
            "celllabel_quant_stackedbar", 
-           "louvain_quant_stackedbar",
+           "leiden_quant_stackedbar",
            "dot_heatmap",
            "dot_heatmap_split",
            "dot_heatmap_split_greyscale",
