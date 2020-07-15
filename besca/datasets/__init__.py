c993eb7777b7a4f987aa52c8302d4b61e33e2333
luisw <luisseverin.wyss@gmail.com>
2020-07-15 12:45:03 +0200

    Added further dataset loading functions. Warning: not complete as I do not have access to all necessary datasets.

--- besca/datasets/__init__.py
+++ besca/datasets/__init__.py
@@ -1,4 +1,4 @@
-from ._datasets import pbmc3k_raw, pbmc3k_filtered, pbmc3k_processed, Smillie2019_raw, Smillie2019_processed, Martin2019_raw, Martin2019_processed, Haber2017_raw, Haber2017_processed, load_immune_signatures, Kotliarov2020_raw, Kotliarov2020_processed, Granja2019_processed
+from ._datasets import pbmc3k_raw, pbmc3k_filtered, pbmc3k_processed, Smillie2019_raw, Smillie2019_processed, Martin2019_raw, Martin2019_processed, Haber2017_raw, Haber2017_processed, load_immune_signatures, Kotliarov2020_raw, Kotliarov2020_processed, Granja2019_raw, Granja2019_processed, Baron2016_processed, Segerstolpe2016_processed, Peng2019_processed
 from ._mito import get_mito_genes
 
 __all__ = ["pbmc3k_raw",
@@ -14,5 +14,9 @@ __all__ = ["pbmc3k_raw",
            "load_immune_signatures",
 	   "Kotliarov2020_raw",
 	   "Kotliarov2020_processed",
-	   "Granja2019_processed"
+	   "Granja2019_raw",
+	   "Granja2019_processed",
+	   "Baron2016_processed",
+	   "Segerstolpe2016_processed",
+	   "Peng2019_processed"
            ]
