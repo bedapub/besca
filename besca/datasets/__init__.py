e1c3c6956b0cd210f2dcbfc9262257836739e1dd
Alice Julien-Laferriere <alice.julien-laferriere@roche.com>
2020-07-31 10:13:22 +0200

    Adding datasets download from Klotiarov

--- besca/datasets/__init__.py
+++ besca/datasets/__init__.py
@@ -1,4 +1,5 @@
 from ._datasets import (Granja2019_citeSeq, Granja2019_processed, Granja2019_raw,
+                        Kotliarov2020_raw, Kotliarov2020_processed,
                         Haber2017_processed, Haber2017_raw,
                         Martin2019_processed, Martin2019_raw,
                         Smillie2019_processed, Smillie2019_raw,
@@ -19,5 +20,7 @@ __all__ = ["pbmc3k_raw",
            "Granja2019_processed",
            "Granja2019_raw",
            "get_mito_genes",
+           "Kotliarov2020_raw", 
+           "Kotliarov2020_processed",
            "load_immune_signatures"
            ]
