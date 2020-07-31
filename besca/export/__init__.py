d4738586cc48d0a75a7a033336168adcccc68156
Alice Julien-Laferriere <alice.julien-laferriere@roche.com>
2020-07-31 17:12:27 +0200

    removing full path in arguments that comes up during documentation compilation. Delete the louvain function, redudancy with the clustering one.

--- besca/export/__init__.py
+++ besca/export/__init__.py
@@ -1,13 +1,12 @@
-from ._export import X_to_mtx, raw_to_mtx, louvain, clustering, labeling, analysis_metadata, ranked_genes, labeling_info, generate_gep, pseudobulk
+from ._export import X_to_mtx, raw_to_mtx, clustering, labeling, analysis_metadata, ranked_genes, labeling_info, generate_gep, pseudobulk
 
 __all__ = ["X_to_mtx",
            "raw_to_mtx", 
-           "louvain", 
            "clustering",
            "labeling", 
            "labeling_info", 
            "analysis_metadata",
            "generate_gep", 
            "ranked_genes",
-           "pseudobulk",
+           "pseudobulk"
            ]
