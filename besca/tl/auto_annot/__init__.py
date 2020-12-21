dee845927ca0669b11d2fe5a399fa1cda6718848
Alice Julien-Laferriere <alice.julien-laferriere@roche.com>
2020-12-21 13:19:50 +0100

    Adding back report functions (from commit c409a0db07f4c5591ae91ce265f88fbf465ac84a)

--- besca/tl/auto_annot/__init__.py
+++ besca/tl/auto_annot/__init__.py
@@ -1,7 +1,3 @@
-from ._auto_annot import read_data, read_raw, read_adata, merge_data, naive_merge, \
-        scanorama_merge, remove_genes, intersect_genes, remove_nonshared, fit, linear_svm, \
-        rbf_svm, sgd_svm, random_forest, logistic_regression, logistic_regression_ovr, logistic_regression_elastic, adata_predict, predict,  adata_pred_prob, predict_proba
+from ._auto_annot import read_data, read_raw, read_adata, merge_data, naive_merge, scanorama_merge, remove_genes, intersect_genes, remove_nonshared, fit, linear_svm, rbf_svm, sgd_svm, random_forest, logistic_regression, logistic_regression_ovr, logistic_regression_elastic, adata_predict, predict, report,  plot_confusion_matrix, adata_pred_prob, predict_proba
 
-__all__ = ['read_data', 'read_raw', 'read_adata', 'merge_data', 'naive_merge', 'scanorama_merge', 'remove_genes', 'intersect_genes', 'remove_nonshared',
-           'fit', 'linear_svm', 'rbf_svm', 'sgd_svm', 'random_forest', 'logistic_regression','logistic_regression_ovr',
-            'logistic_regression_elastic', 'adata_predict', 'predict',  'adata_pred_prob', 'predict_proba']
+__all__ = ['read_data', 'read_raw', 'read_adata', 'merge_data', 'naive_merge', 'scanorama_merge', 'remove_genes', 'intersect_genes', 'remove_nonshared', 'fit', 'linear_svm', 'rbf_svm', 'sgd_svm', 'random_forest', 'logistic_regression','logistic_regression_ovr', 'logistic_regression_elastic', 'adata_predict', 'predict', 'report', 'plot_confusion_matrix', 'adata_pred_prob', 'predict_proba']
--- besca/tl/auto_annot/_auto_annot.py
+++ besca/tl/auto_annot/_auto_annot.py
@@ -1,21 +1,23 @@
-import csv
 import sys
-
-import numpy as np
-import pandas as pd
-import scanorama as scan
-import scanpy as sc
+import os
+import csv
 import scipy
+import scanpy as sc
 import scvelo as scv
-from sklearn.calibration import CalibratedClassifierCV
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn.svm import LinearSVC,SVC
+from sklearn.linear_model import SGDClassifier, LogisticRegressionCV, LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import (LogisticRegression, LogisticRegressionCV,
-                                  SGDClassifier)
-from sklearn.model_selection import (GridSearchCV, StratifiedShuffleSplit,
-                                     cross_validate, train_test_split)
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC, LinearSVC
+from sklearn.metrics import f1_score, make_scorer, accuracy_score, classification_report, confusion_matrix
 from sklearn.utils.multiclass import unique_labels
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.model_selection import cross_validate
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit, GridSearchCV
+import scanorama as scan 
 
 
 def read_data(train_paths, train_datasets, test_path, test_dataset, use_raw = False):
@@ -639,6 +641,191 @@ def predict(classifier, scaler, adata_pred, threshold = 0):
     return results 
 
 
+def report(adata_pred, celltype, method, analysis_name, train_datasets, test_dataset, merge, use_raw = False, genes_to_use = 'all', remove_nonshared = False, clustering = 'leiden', asymmetric_matrix = True):
+    """ reports basic metrics, produces confusion matrices and plots umap of prediction
+
+    Writes out a csv file containing all accuracy and f1 scores.
+    Writes normalized and absolute confusion matrices, as well as umap prediction comparisons to ./figures.
+    
+    parameters
+    ----------
+    adata_pred: AnnData
+        original adata object with 'auto_annot' column
+    celltype: `str`
+        celltype column on which the prediction was performed
+    method: `str`
+        method that was used for prediction.
+    analysis_name: `str`
+        name of the analyis, used for writing files
+    train_datasets: `list`
+        list of used training datasets
+    test_dataset: `str`
+        name of test dataset
+    merge: `str`
+        what merging was performed
+    use_raw: `bool`  | default = False
+        if anndata.raw was used
+    genes_to_use: `list` or `string` | default = 'all'
+        what geneset wsa used
+    remove_nonshared: `bool`|default = False
+    clustering: `str` | default = leiden
+        clustering that was used in original analysis of testing set, needed for umap plotting
+    asymmetric_matrix: `bool` | default = True
+        if False returns square confusion matrix, if True it only shows possible combinations
+
+    returns
+    -------
+    0
+    """
+
+    # calculate umaps for plot
+    if 'X_umap' not in adata_pred.obsm:
+        sc.tl.umap(adata)
+
+
+    #get acc
+    acc = accuracy_score(adata_pred.obs[celltype], adata_pred.obs['auto_annot'])
+
+    #get f1
+    f1 = f1_score(adata_pred.obs[celltype], adata_pred.obs['auto_annot'], labels = adata_pred.obs[celltype], average = 'macro')
+
+    #get report
+        
+    report = classification_report(adata_pred.obs[celltype], adata_pred.obs['auto_annot'], output_dict=True)
+    sklearn_report = pd.DataFrame(report).transpose()
+    
+    #csv file with important metrics
+    with open('auto_annot_report_'  + analysis_name +'.csv', mode='w') as report:
+        report_writer = csv.writer(report, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        report_writer.writerow(['train_dataset = ', train_datasets, 'test_datset = ', test_dataset])
+        report_writer.writerow(['celltype = ', celltype, 'method = ', method])
+        report_writer.writerow(['remove_nonshared = ', remove_nonshared, 'merge = ', merge, 'use_raw = ', use_raw, 'genes_to_use = ', genes_to_use])
+
+        report_writer.writerow(['accuracy=', acc, 'f1=', f1])
+
+        report_writer.writerow(['classification report'])
+        sklearn_report.to_csv(report, header = True)
+        
+        
+        # make umap
+        sc.settings.set_figure_params(dpi=240)
+
+        sc.pl.umap(adata_pred, color=[celltype, 'auto_annot', clustering], legend_loc='on data',legend_fontsize=7, save= '.ondata_'+ analysis_name + '.png')
+        sc.pl.umap(adata_pred, color=[celltype, 'auto_annot', clustering],legend_fontsize=7, wspace = 1.5,  save = '.' + analysis_name + '.png')
+        sc.settings.set_figure_params(dpi=60)
+
+        # make conf matrices (4)
+        class_names =  np.unique(np.concatenate((adata_pred.obs[celltype], adata_pred.obs['auto_annot'])))
+        np.set_printoptions(precision=2)
+        # Plot non-normalized confusion matrix
+        plot_confusion_matrix(adata_pred.obs[celltype], adata_pred.obs['auto_annot'], classes=class_names, celltype=celltype,  title='Confusion matrix, without normalization',numbers = False, adata_predicted = adata_pred, asymmetric_matrix = asymmetric_matrix)
+        plt.savefig(os.path.join('./figures/SVM_confusion_matrix_' + analysis_name +'_' + celltype +  '.svg'))
+
+        # Plot normalized confusion matrix with numbers
+        plot_confusion_matrix(adata_pred.obs[celltype], adata_pred.obs['auto_annot'], classes=class_names,celltype=celltype,  normalize=True, title='Normalized confusion matrix', numbers = False, adata_predicted = adata_pred, asymmetric_matrix = asymmetric_matrix)
+        plt.savefig(os.path.join('./figures/SVM_confusion_matrix_norm_' + analysis_name +'_' + celltype +  '.svg'))
+
+    
+def plot_confusion_matrix(y_true, y_pred, classes, celltype,
+                          normalize=False,
+                          title=None, numbers =False,
+                          cmap=plt.cm.Blues, adata_predicted= None, asymmetric_matrix = True): 
+    """ plots confusion matrices
+
+    returns a matplotlib confusion matrix
+    
+    parameters
+    ----------
+    y_true: pandas.core.series.Series
+
+        ordered series of all true labels
+    y_pred: pandas.core.series.Series
+        ordered series of all predicted celltypes
+    classes: numpy.ndarray
+        union of true and predictable celltypes
+    celltype: `str`
+        celltype column on which the prediction was performed
+    normalize: `bool` | default = False
+        whether to return absolute values or to value all celltypes equally
+    title: `str` | default = None
+        title to be given to confusion matrix figure in file.
+    numbers: `bool`| default = False
+        should the numbers be displayed in the plot. Note: is illegible in larger plots
+    cmap: matplotlib.cm | default = plt.cm.Blues
+        colour to be used for plotting
+    asymmetric_matrix: `bool` | default = True
+        if False returns square confusion matrix, if True it only shows possible combinations
+
+    returns
+    -------
+    matplotlib.pyplot.plot
+        plot of confusion matrix
+    """
+    matplotlib.use('Agg')
+    
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    #classes = classes[unique_labels(y_true, y_pred)]
+    if asymmetric_matrix == True:
+        class_names =  np.unique(np.concatenate((adata_predicted.obs[celltype], adata_predicted.obs['auto_annot'])))
+        class_names_orig = np.unique(adata_predicted.obs[celltype])
+        class_names_pred = np.unique(adata_predicted.obs['auto_annot'])
+        test_celltypes_ind = np.searchsorted(class_names, class_names_orig)
+        train_celltypes_ind = np.searchsorted(class_names, class_names_pred)
+        cm=cm[test_celltypes_ind,:][:,train_celltypes_ind]
+    
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    fig, ax = plt.subplots(figsize=(15,15))
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax, shrink = 0.8)
+    # We want to show all ticks...
+    if asymmetric_matrix == True:
+        ax.set(xticks=np.arange(cm.shape[1]),
+               yticks=np.arange(cm.shape[0]),
+               # ... and label them with the respective list entries
+               xticklabels=class_names_pred, yticklabels=class_names_orig,
+               title=title,
+               ylabel='True label',
+               xlabel='Predicted label')
+    else:
+        ax.set(xticks=np.arange(cm.shape[1]),
+               yticks=np.arange(cm.shape[0]),
+               # ... and label them with the respective list entries
+               xticklabels=classes, yticklabels=classes,
+               title=title,
+               ylabel='True label',
+               xlabel='Predicted label')
+        
+    ax.grid(False)
+    #ax.tick_params(axis='both', which='major', labelsize=10)
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    if numbers == True:
+        fmt = '.2f' if normalize else 'd'
+        thresh = cm.max() / 2.
+        for i in range(cm.shape[0]):
+            for j in range(cm.shape[1]):
+                ax.text(j, i, format(cm[i, j], fmt),
+                        ha="center", va="center",
+                        color="white" if cm[i, j] > thresh else "black")
+    #fig.tight_layout()
+    return ax
 
 def adata_pred_prob(classifier, scaler,adata_pred, adata_orig, threshold = 0):
     """ predicts on testing set using trained classifier and returns class probability for every cell and every class
