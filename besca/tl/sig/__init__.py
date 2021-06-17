5cf4aaf8e872ca27b5d031f1e1349c59884a033a
Petra Schwalie <schwalip@rkalbhpc014.kau.roche.com>
2021-06-17 15:21:08 +0200

    Added sig export function and added sig output example

--- besca/tl/sig/__init__.py
+++ besca/tl/sig/__init__.py
@@ -2,7 +2,7 @@
 from ._gems_link import (get_gems, insert_gems, get_similar_geneset)
 from ._annot import (add_anno, getset, make_anno, match_cluster,
                      obtain_dblabel, read_annotconfig, score_mw, export_annotconfig)
-from ._io_sig import read_GMT_sign, convert_to_directed
+from ._io_sig import read_GMT_sign, convert_to_directed, write_gmtx_forgems
 from ._sig import (combined_signature_score, compute_signed_score,
                    filter_siggenes, make_gmtx)
 
@@ -22,4 +22,5 @@ __all__ = ['combined_signature_score',
            'get_similar_geneset',
            'export_annotconfig',
            'convert_to_directed', 
-           'make_gmtx']
+           'make_gmtx',
+          'write_gmtx_forgems']
