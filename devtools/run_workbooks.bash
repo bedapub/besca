344172a6eeecba169a0e12ceafcf2743d5895265
Jitao David Zhang <jitao_david.zhang@roche.com>
2022-03-03 14:30:06 +0100

    add running the minimalistic version

--- devtools/run_workbooks.bash
+++ devtools/run_workbooks.bash
@@ -7,7 +7,7 @@
 ## 2. the conda/venv environment for besca development is activated
 ## 3. besca_dev kernel has been installed (see install_besca_anew_local.bash)
 
-for notebook in "standard_workflow_besca2.ipynb" "celltype_annotation_besca.ipynb" "Signature_exports.ipynb" "Testing_Notebook.ipynb"; do
+for notebook in "minimal_notebook.ipynb" "standard_workflow_besca2.ipynb" "celltype_annotation_besca.ipynb" "Signature_exports.ipynb" "Testing_Notebook.ipynb"; do
     echo Running notebook "$notebook"
     jupyter nbconvert --to notebook --execute workbooks/"$notebook"
     jupyter nbconvert --clear-output workbooks/"$notebook"
