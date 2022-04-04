7ce85ef3752323a7c56cc3efae3e46fddc1bb965
hatjek <hatjek@users.noreply.github.com>
2022-04-04 13:42:34 +0200

    Update _celltype_quantification.py
    Added option to plot absolute cell counts instead of percentages. Added figure size parameters.

--- besca/pl/_celltype_quantification.py
+++ besca/pl/_celltype_quantification.py
@@ -126,8 +126,8 @@ def celllabel_quant_boxplot(
     return fig
 
 
-def celllabel_quant_stackedbar(adata, subset_variable, count_variable="celltype"):
-    """Generate a stacked bar plot of the percentage of labelcounts within each AnnData subset
+def celllabel_quant_stackedbar(adata, subset_variable, count_variable="celltype", plot_percentages=True, myh=4, myw=8):
+    """Generate a stacked bar plot of the percentage or absolute of labelcounts within each AnnData subset
 
     parameters
     ----------
@@ -137,7 +137,13 @@ def celllabel_quant_stackedbar(adata, subset_variable, count_variable="celltype"
         string identifying the column name of adata.obs along which the AnnData object should be subsetted
     count_variable: `str` | default  = 'celltype'
         string identiyfing the column of adata.obs containing the labels to be counted
-
+    plot_percentages: `bool` | default = True
+        boolian indicating if the percentages or the total counts should be plotted
+    myh: `integer` | default = 4
+        height of the figure
+    myw: `integer` | default = 8
+        width of the figure
+            
     returns
     -------
     Figure
@@ -160,20 +166,25 @@ def celllabel_quant_stackedbar(adata, subset_variable, count_variable="celltype"
     counts_celltype = count_occurrence_subset(
         adata, subset_variable, count_variable=count_variable, return_percentage=False
     )
-    percentages = DataFrame(
+    values = DataFrame(
         index=natsorted(counts_celltype.index.tolist()),
         columns=counts_celltype.columns.tolist(),
     )
 
     for cell in counts_celltype.index.tolist():
         data = counts_celltype.loc[cell].tolist()
-        percentage = [x / sum(data) for x in data]
-        percentages.loc[cell] = percentage
+        if plot_percentages:
+            data = [x / sum(data) for x in data]
+        values.loc[cell] = data
 
-    fig = percentages.plot(kind="bar", stacked=True, figsize=(8, 4))
-    fig.set_ylabel("percentage")
+    fig = values.plot(kind="bar", stacked=True, figsize=(myw, myh))
+    if plot_percentages:
+        fig.set_ylabel("percentage")
+    else:
+        fig.set_ylabel("number of cells")
+            
     fig.legend(bbox_to_anchor=(1, 1))
-
+     
     # fix figure axis to include legend
     tight_layout()
 
