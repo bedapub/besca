04d57347c74814a94f0d577d65786e72a61bf7c9
paul.geser <paul.geser@roche.com>
2022-04-12 06:26:40 -0400

    changed doctests

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+whereis conda
 python -V
 conda init bash
 source ~/.bashrc
+source ~/anaconda3/etc/profile.d/conda.sh
 conda activate besca_dev
 ls
 pytest --doctest-modules -W ignore::PendingDeprecationWarning
